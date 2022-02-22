e581f7a5c727617f87f6414183ca06c1d346e14b
Admin 水野 <mizno911.zeta@gmail.com>
2022-02-22 20:42:29 +0900

    auth関連

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->to('/home');
+});
+
+Route::get('/error', function () {
+    return 'not login';
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('/home');
