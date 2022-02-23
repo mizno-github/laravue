13267683d2a464d3a5fe2d3d08eb7a1d83d0e2c0
Admin 水野 <mizno911.zeta@gmail.com>
2022-02-23 19:17:49 +0900

    バックエンドのパス変更

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,6 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('/home');
 
-Route::view('/modal', 'modal');
+Route::prefix('design/')->group(function () {
+    Route::view('modal', 'design/modal');
+});
